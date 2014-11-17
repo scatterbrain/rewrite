bb153672a87060714fe1d1550cef14339c9c4183
Mikko Hämäläinen <mikko.hamalainen@greyarealabs.com>
2014-11-17 21:44:06 +0200

    Testing out nodegit which seems to be heavily under development still

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     router = express.Router(), 
-    React = require('react');
+    React = require('react'), 
+    Git = require('nodegit'), 
+    util = require('util');
 
 // so that you are able to require() jsx files
 var jsx = require('node-jsx');
@@ -15,6 +17,21 @@ router.get('/', function(req, res) {
     var commentContents = React.renderToString(comments(props));
     var editorContents = React.renderToString(editor({}));
     res.render('index', { title: 'Express', content : commentContents, editor : editorContents });
+});
+
+router.get('/repo', function(req, res) {
+    console.log("Call repository init " + util.inspect(Git.Repository.init("/Users/mbhamala/tmp/", false).then(function(err, repo) { return 1; })));
+    Git.Repository.init("/Users/mbhamala/tmp/", false) 
+        .then(function(err, repo) {
+            console.log("Repor cre"); 
+        });
+
+    // Clone a given repository into a tmp folder.
+    Git.Clone.clone("git://github.com/nodegit/nodegit", "tmp").then(function(repo) {
+        console.log("Cloned");
+    });
+
+    res.render('index', {title : 'Repo created'});
 });
 
 module.exports = router;
