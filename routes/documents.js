01b51a3553ae0d0ba1936ff1a2ed2d9971faa52e
Mikko Hämäläinen <mikko.hamalainen@greyarealabs.com>
2014-12-08 17:58:00 +0200

    Started git document reading/saving

--- routes/documents.js
+++ routes/documents.js
@@ -1,21 +1,57 @@
 var express = require('express'), 
     router = express.Router(),
-    util = require('util'), 
+    util = require('util'),
+    Remote = require('../libs/remote'),     
     //Temporarily use the WriteStore on the server side as well
     WriteStore = require('../public/javascripts/stores/write_store.js');
 
 router.get('/', function(req, res) {
-    var data = WriteStore.getDocument(); 
-    res.setHeader('Content-Type', 'application/json');    
-    res.send(JSON.stringify(data));
+    var remote, cmd, replyDoc;
+    //Read from git
+    cmd = { cmd : "read" };
+    remote = Remote.createRemote('doc.request');
+    //Remote connection established, write request
+    remote.on('ready', function() {
+        remote.write(JSON.stringify(cmd));
+    });
+    //When we receive reply data
+    remote.on('data', function(data) {
+      data = JSON.parse(data);
+
+      replyDoc = WriteStore.getDocument();       
+      if (data.result === 0) {
+        replyDoc.text = data.doc.text; 
+      } else {
+        console.log(util.inspect(data));
+      } 
+
+      res.setHeader('Content-Type', 'application/json');    
+      res.send(JSON.stringify(replyDoc));
+      
+      remote.close();
+    });
+    remote.connect();
 });
 
 router.post('/', function(req, res) {
-    var validatedDocument;
+    var validatedDocument, rabbitDoc, remote;
     WriteStore.receiveDocument(req.body);
     validatedDocument = WriteStore.getDocument(); 
-    res.setHeader('Content-Type', 'application/json');    
+    res.setHeader('Content-Type', 'application/json');   
     res.send(JSON.stringify(validatedDocument));
+
+    //Save to git
+    rabbitDoc = { cmd : "commit", doc : validatedDocument};
+    remote = Remote.createRemote('doc.request');
+    //Remote connection established, write request
+    remote.on('ready', function() {
+        remote.write(JSON.stringify(rabbitDoc));
+    });
+    //When we receive reply data
+    remote.on('data', function(data) {
+        remote.close();
+    });
+    remote.connect();
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     router = express.Router(), 
-    React = require('react'), 
-    Remote = require('../libs/remote');
+    React = require('react');
 
 // so that you are able to require() jsx files
 var jsx = require('node-jsx');
@@ -11,18 +10,6 @@ var editor = React.createFactory(require('./../public/javascripts/components/mar
 
 /* GET home page. */
 router.get('/', function(req, res) {
-
-    //RabbitMQ test
-    var remote = Remote.createRemote('doc.request');
-    //Remote connection established, write request
-    remote.on('ready', function() {
-        remote.write(JSON.stringify({welcome: 'rabbit.js'}));
-    });
-    //When we receive reply data
-    remote.on('data', function() {
-        remote.close();
-    });
-    remote.connect();
 
     var props = {};
     var editorContents = React.renderToString(editor(props));
